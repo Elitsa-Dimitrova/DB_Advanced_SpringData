29381e32d7fe714f15afabd200b35bba367884af
Elitsa-Dimitrova <39455900+Elitsa-Dimitrova@users.noreply.github.com>
2020-02-29 19:03:31 +0200

    Task 4: Create Entity Manager

---
+++ demoMaven/src/main/java/ORM/EntityManager.java
@@ -0,0 +1,37 @@
+package main.java.ORM;
+
+import java.sql.Connection;
+
+public class EntityManager<E> implements DbContext<E> {
+    private Connection connection;
+
+    public EntityManager(Connection connection) {
+        this.connection = connection;
+    }
+
+    // TODO: Implement Methods
+    @Override
+    public boolean persist(E entity) {
+        return false;
+    }
+
+    @Override
+    public Iterable<E> find(Class<E> table) {
+        return null;
+    }
+
+    @Override
+    public Iterable<E> find(Class<E> table, String where) {
+        return null;
+    }
+
+    @Override
+    public E findFirst(Class<E> table) {
+        return null;
+    }
+
+    @Override
+    public E findFirst(Class<E> table, String where) {
+        return null;
+    }
+}
