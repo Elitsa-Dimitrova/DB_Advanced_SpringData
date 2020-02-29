71938534887820b1f624eac4052ce35ec3ab6776
Elitsa-Dimitrova <39455900+Elitsa-Dimitrova@users.noreply.github.com>
2020-02-29 18:48:25 +0200

    Task 3: 3.Create Database Context

---
+++ demoMaven/src/main/java/ORM/DbContext.java
@@ -0,0 +1,10 @@
+package main.java.ORM;
+
+public interface DbContext<E> {
+    boolean persist(E entity);
+
+    Iterable<E> find(Class<E> table);
+    Iterable<E> find(Class<E> table, String where);
+    E findFirst(Class<E> table);
+    E findFirst(Class<E> table, String where);
+}
